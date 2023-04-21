31385db38b23be967394b33807313b9e7991404a
Amber Atkins <ambera1009@outlook.com>
2023-04-21 15:13:11 -0400

    Linux inst cleanup, tcpdump and nmap work.

--- linux_installer.sh
+++ linux_installer.sh
@@ -1,10 +1,57 @@
 #!/bin/bash
 
+#TODO: add dnf/rpm/pacman pkg managers?
+
 packages=(
 	nmap
 	wireshark
 	tcpdump
+	libpcap
+	flex
+	bison
 )
+
+
+
+function tarballpack(){
+	SECONDS=0
+	#make installer dir
+	mkdir ~/Desktop/Installers
+	cd ~/Desktop/Installers || return
+	#nmap
+	wget https://nmap.org/dist/nmap-7.93.tgz
+	#wireshark
+	wget https://www.wireshark.org/download/src/wireshark-latest.tar.xz 
+	#tcpdump
+	wget https://www.tcpdump.org/release/tcpdump-4.99.3.tar.gz
+	#libpcap
+	wget https://www.tcpdump.org/release/libpcap-1.10.4.tar.gz 
+	#libpcap dependencies
+	wget https://github.com/westes/flex/files/981163/flex-2.6.4.tar.gz
+	wget http://ftp.gnu.org/gnu/bison/bison-3.8.tar.xz
+	#unpacking tarballs
+	tar -zxf nmap-7.93.tgz
+	tar -xf wireshark-latest.tar.xz
+	tar -zxf tcpdump-4.99.3.tar.gz
+	tar -zxf libpcap-1.10.4.tar.gz
+	tar -zxf flex-2.6.4.tar.gz
+	tar -xf bison-3.8.tar.xz
+	rm nmap-7.93.tgz wireshark-latest.tar.xz tcpdump-4.99.3.tar.gz libpcap-1.10.4.tar.gz flex-2.6.4.tar.gz bison-3.8.tar.xz
+	#these are very noisy, maybe pipe the stderr/stdout output somewhere?
+	cd flex-2.6.4
+	./configure && make -s && sudo make install
+	cd ../bison-3.8
+	./configure && make -s && sudo make install
+	cd ../libpcap-1.10.4
+	./configure && make -s && sudo make install
+	cd ../tcpdump-4.99.3
+	./configure && make -s && sudo make install
+	cd ../nmap-7.93
+	./configure && make -s && sudo make install
+	cd ../wireshark-*
+	 cmake && make && sudo make install
+	echo "Installation finished. Time elasped: $SECONDS seconds."
+}
 
 function packrun(){
 echo "Detecting your package manager..."
@@ -30,25 +77,7 @@ if [ "$packmanager" != "NULL" ]; then
 		sudo $packmanager install -y "$pack"
 	done
 else
-	#make installer dir
-	mkdir ~/Desktop/Installers
-	cd ~/Desktop/Installers
-	#nmap
-	wget https://nmap.org/dist/nmap-7.93.tgz
-	#wireshark
-	wget https://www.wireshark.org/download/src/wireshark-latest.tar.xz 
-	#tcpdump
-	wget https://www.tcpdump.org/release/tcpdump-4.99.3.tar.gz
-	#unpacking tarballs
-	tar -zxvf nmap-7.93.tgz
-	tar -xvf wireshark-latest.tar.xz
-	tar -zxvf tcpdump-4.99.3.tar.gz
-	### TEMP do these exist ###
-if [ -e nmap-7.93 ]; then
-    echo "The dir exists."
-else
-    echo "The dir does not exist."
-fi
+	tarballpack
 fi
 }
 
@@ -56,10 +85,10 @@ fi
 echo "The following packages will be installed: "
 for pack in "${packages[@]}" 
 	do
-		printf "$pack\n"
+		printf "%s\n", "$pack"
 	done
 echo "Do you want to continue? [Y/n]"
-read choice
+read -r choice
 
 case "$choice" in
 	'Y' | 'y' )
